1949ddf4913412d0a88d6d57169bcbcc9a4ad646
luis pacheco <laps1308@gmail.com>
2022-09-09 18:01:22 -0500

    modify config file again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root:"./",
+  base:"/",
+  publicDir:"public",
+  build:{
+    outDir:"build",
+    assetsDir:"assets"
+  },
   plugins: [react()],
   
 })
