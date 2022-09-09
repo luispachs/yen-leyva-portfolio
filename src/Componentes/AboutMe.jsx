b7a6ccc5e0a72a6a021fc8a9365cac6aa4975413
luis pacheco <laps1308@gmail.com>
2022-09-09 17:07:18 -0500

    Modification to day 09092922 delete images that no needed add image for background ans images waves add font Roboto and Style script change global styles

---
+++ src/Componentes/AboutMe.jsx
@@ -0,0 +1,13 @@
+import Layout from "./Layout";
+
+function AboutMe(){
+
+    return (
+        <Layout>
+            <h2> este es About me</h2>
+        </Layout>
+    )
+
+}
+
+export default AboutMe;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {BrowserRouter, Routes,Route} from "react-router-dom"
+import Home  from "./Componentes/Home"
+import AboutMe from './Componentes/AboutMe'
+import MyWorks from './Componentes/MyWorks'
+import Contactme from './Componentes/ContactMe'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path="/aboutme" element={<AboutMe/>}></Route>
+      <Route path='/myworks' element={<MyWorks/>}></Route>
+      <Route path='/contactme' element={<Contactme/>}></Route>
+    </Routes>
+  </BrowserRouter>
 )
